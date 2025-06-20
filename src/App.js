65778ed31ae354df01406071a220e167f006ec70
Lorenzo Holmes <02k20AD@gmail.com>
2025-06-20 17:59:43 -0500

    Updated app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // src/App.js
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import FCTLandingPage from "./pages/FCTLandingPage";
 import ScrollToTop from "./utils/ScrollToTop";
 import NavBar from "./components/NavBar";
